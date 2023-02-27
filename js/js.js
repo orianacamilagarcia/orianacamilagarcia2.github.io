581d75a3952db740c1a2f404da7d9a4b8dd1b0d6
Camila <orianacamilagarcia@gmail.com>
2023-02-27 01:43:30 -0300

    hoja de vida

---
+++ js/js.js
@@ -0,0 +1,12 @@
+document.getElementById('boton').onclick = function () {
+  console.log("Capturamos el evento click");    
+  document.getElementById("demo").innerHTML  =  "@DanielaOrtiz91"
+}
+
+document.getElementById('botonn').onclick = function () {
+    console.log("Capturamos el evento click");    
+    document.getElementById("demo").innerHTML  =  "@Danni_Ortizz"
+  }
+
+
+    
